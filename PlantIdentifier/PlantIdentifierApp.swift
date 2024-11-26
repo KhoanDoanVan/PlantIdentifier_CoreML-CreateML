0a9f4532a4fece7af9fd21e688a27a288796a589
khoanisdev <doanvankhoan124@gmail.com>
2024-11-26 20:16:55 +0700

    Initial Commit

---
+++ PlantIdentifier/PlantIdentifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantIdentifierApp.swift
+//  PlantIdentifier
+//
+//  Created by Đoàn Văn Khoan on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PlantIdentifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
